d44231b0e1a9f5d6c90c6b56e131873c0c9cc5b3
lakshay170 <123258423+lakshay170@users.noreply.github.com>
2023-01-30 21:52:56 +0530

    Kunal updated even odd later
    Description is added here.And finally its committed

--- evenOrOdd.js
+++ evenOrOdd.js
@@ -1,7 +1,7 @@
 let a=45
 if(a%2==0){
-    console.log('even')
+    console.log('even') //comments added later can be seen in history in red color
 }
 else{
-    console.log('odd')
+    console.log('odd')  //
 }
